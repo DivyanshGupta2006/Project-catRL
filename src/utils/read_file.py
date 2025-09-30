129e7ebb25999c3b363038c43abdfa228140bd95
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2025-09-30 22:39:43 +0530

    Fix bugs

--- src/utils/read_file.py
+++ src/utils/read_file.py
@@ -3,19 +3,8 @@ from src.utils import get_absolute_path, get_config
 
 config = get_config.read_yaml()
 
-def read_raw_data(file) -> pd.DataFrame:
-    dir = get_absolute_path.absolute(config['paths']['raw_data_directory'])
-    print(f"Reading Raw Equity data: {file} !")
-    try:
-        data = pd.read_csv(f'{dir}/{file}.csv')
-        print(f"Successfully read: {file} !")
-        return data
-    except Exception as e:
-        print(f"Failed to read {file}: {e} !")
-        return None
-
-def read_processed_data(file) -> pd.DataFrame:
-    dir = get_absolute_path.absolute(config['paths']['processed_data_directory'])
+def read_data(file) -> pd.DataFrame:
+    dir = get_absolute_path.absolute(config['paths']['data_directory'])
     print(f"Reading Raw Equity data: {file} !")
     try:
         data = pd.read_csv(f'{dir}/{file}.csv')
