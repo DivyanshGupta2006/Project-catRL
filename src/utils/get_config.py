c5a2c169a9a7c0d239b10bedbdef2e107fee8e41
Aayush Sharma <khandelwal.aayush1236@gmail.com>
2025-10-02 14:41:49 +0530

    __get_project_root().joinpath__ changed to __absolute__

--- src/utils/get_config.py
+++ src/utils/get_config.py
@@ -2,7 +2,7 @@ import yaml
 from src.utils import get_absolute_path
 
 def read_yaml() -> dict:
-    config_path = get_absolute_path.get_project_root().joinpath('config.yaml')
+    config_path = get_absolute_path.absolute('config.yaml')
     with open(config_path, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     return config
