252928c73ce93be0058cd7b5e5c6ec6c4be877e6
Aayush Sharma <khandelwal.aayush1236@gmail.com>
2025-10-03 15:55:44 +0530

    added read_featured_data function, minor fixes

--- src/utils/read_file.py
+++ src/utils/read_file.py
@@ -6,7 +6,7 @@ config = get_config.read_yaml()
 def read_raw_data(symbol) -> pd.DataFrame:
     dir = get_absolute_path.absolute(config['paths']['raw_data_directory'])
     file = symbol.split('/')[0]
-    print(f"Reading Raw Equity data: {file} !")
+    print(f"Reading Raw data: {file} !")
     try:
         data = pd.read_csv(f'{dir}/{file}.csv', index_col=[0])
         print(f"Successfully read: {file} !")
@@ -18,7 +18,7 @@ def read_raw_data(symbol) -> pd.DataFrame:
 def read_training_data(symbol) -> pd.DataFrame:
     dir = get_absolute_path.absolute(config['paths']['train_data_directory'])
     file = symbol.split('/')[0]
-    print(f"Reading Raw Equity data: {file} !")
+    print(f"Reading Training data: {file} !")
     try:
         data = pd.read_csv(f'{dir}/{file}.csv', index_col=[0])
         print(f"Successfully read: {file} !")
@@ -30,7 +30,7 @@ def read_training_data(symbol) -> pd.DataFrame:
 def read_test_data(symbol) -> pd.DataFrame:
     dir = get_absolute_path.absolute(config['paths']['test_data_directory'])
     file = symbol.split('/')[0]
-    print(f"Reading Raw Equity data: {file} !")
+    print(f"Reading Test data: {file} !")
     try:
         data = pd.read_csv(f'{dir}/{file}.csv', index_col=[0])
         print(f"Successfully read: {file} !")
@@ -42,7 +42,7 @@ def read_test_data(symbol) -> pd.DataFrame:
 def read_val_data(symbol) -> pd.DataFrame:
     dir = get_absolute_path.absolute(config['paths']['val_data_directory'])
     file = symbol.split('/')[0]
-    print(f"Reading Raw Equity data: {file} !")
+    print(f"Reading Validation data: {file} !")
     try:
         data = pd.read_csv(f'{dir}/{file}.csv', index_col=[0])
         print(f"Successfully read: {file} !")
@@ -50,3 +50,16 @@ def read_val_data(symbol) -> pd.DataFrame:
     except Exception as e:
         print(f"Failed to read {file}: {e} !")
         return None
+
+def read_featured_data(symbol) -> pd.DataFrame:
+    dir = get_absolute_path.absolute(config['paths']['featured_data_directory'])
+    file = symbol.split('/')[0]
+    print(f"Reading Featured data: {file} !")
+    try:
+        data = pd.read_csv(f'{dir}/{file}.csv', index_col=[0])
+        print(f"Successfully read: {file} !")
+        return data
+    except Exception as e:
+        print(f"Failed to read {file}: {e} !")
+        return None
+
