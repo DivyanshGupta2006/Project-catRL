d33f6a8efa0f5a2fc07a904dcfee41d1604f5d95
Sarath Chandra <sarathj2006@gmail.com>
2025-10-04 12:04:35 +0530

    optional download + readme changes

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Crypto Algorithmic Trading based on Reinforcement Learning
 Potential Improvements:
  - Train, Validation, Test split.
+ - Detrending during preprocessing of the data
+ - Outlier handling
+ - 
 <br>
 
 Authors:
--- src/update_files/update_data.py
+++ src/update_files/update_data.py
@@ -1,7 +1,9 @@
 from src.data_processing import download_data, preprocess_data, split_data, feature_engineer, link_data
 
 def update():
-    download_data.download()
+    a = input(print('Download Data? : (y/n)'))
+    if a.lower() == 'y':
+        download_data.download()
     split_data.split()
     link_data.link('training-val')
     link_data.link('val-test')
