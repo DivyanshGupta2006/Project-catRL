5d103dc5e3704f880d43ea96f5b8ccf3079b8409
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2025-10-13 18:52:55 +0530

    Fix bugs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-from src.update_files import update_data
+from src.update_files import update_data, update_date
 
 def start():
+    update_date.update()
     choice = input("Would you like to update the data? (y/n): ")
     if choice.lower() == 'y':
         update_data.update()
