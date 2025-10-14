a22c73e7cdae6f09850a45e90594890975a3e50d
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2025-10-14 17:33:50 +0530

    Add data handler

--- src/backtester/data_handler.py
+++ src/backtester/data_handler.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import json
+
+from src.agent import predict
+from src.risk_management import slippage, stop_loss, take_profit
+from src.position_sizing import amount_calculator
 from src.utils import get_config, read_file, get_absolute_path
 
 config = get_config.read_yaml()
@@ -12,3 +16,11 @@ def fetch_data(data):
     with open(dir, 'w') as f:
         json.dump(state, f, indent=4)
     return data.loc[timestep]
+
+def add_predictions(candle):
+    candle = predict.predict_position(candle)
+    candle = slippage.get_order_price(candle)
+    candle = amount_calculator.calculate(candle)
+    candle = stop_loss.get_stop_loss(candle)
+    candle = take_profit.get_take_profit(candle)
+    return candle
