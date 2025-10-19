de663164e7b8c6f504a22478e1227be2a86f0303
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2025-10-20 01:01:20 +0530

    Fix bugs

--- src/backtester/execute_SL_TP.py
+++ src/backtester/execute_SL_TP.py
@@ -10,21 +10,21 @@ def execute(candle):
     for crypto in portfolio.index:
         # shorting
         if portfolio.loc[crypto, 'amt'] < 0:
-            if candle[crypto]['high'] > portfolio.loc[crypto, 'stop_price']:
+            if candle[crypto]['high'] >= portfolio.loc[crypto, 'stop_price']:
                 portfolio.loc[crypto, 'amt'] -= portfolio.loc[crypto, 'stop_portion']
                 state['cash'] += (portfolio.loc[crypto, 'stop_portion'] * portfolio.loc[crypto, 'stop_price'])
 
-            elif candle[crypto]['low'] < portfolio.loc[crypto, 'take_price']:
+            elif candle[crypto]['low'] <= portfolio.loc[crypto, 'take_price']:
                 portfolio.loc[crypto, 'amt'] -= portfolio.loc[crypto, 'take_portion']
                 state['cash'] += (portfolio.loc[crypto, 'take_portion'] * portfolio.loc[crypto, 'take_price'])
 
         # longing
         elif portfolio.loc[crypto, 'amt'] > 0:
-            if candle[crypto]['low'] < portfolio.loc[crypto, 'stop_price']:
+            if candle[crypto]['low'] <= portfolio.loc[crypto, 'stop_price']:
                 portfolio.loc[crypto, 'amt'] -= portfolio.loc[crypto, 'stop_portion']
                 state['cash'] += (portfolio.loc[crypto, 'stop_portion'] * portfolio.loc[crypto, 'stop_price'])
 
-            elif candle[crypto]['high'] > portfolio.loc[crypto, 'take_price']:
+            elif candle[crypto]['high'] >= portfolio.loc[crypto, 'take_price']:
                 portfolio.loc[crypto, 'amt'] -= portfolio.loc[crypto, 'take_portion']
                 state['cash'] += (portfolio.loc[crypto, 'take_portion'] * portfolio.loc[crypto, 'take_price'])
 
