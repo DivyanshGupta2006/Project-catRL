Fixed normalization

src/strategy/model.py
@@ -7,32 +7,35 @@ from src.utils import get_config
 config = get_config.read_yaml()
 num_assets = len(config['data']['symbols']) + 1
 
-class ActorCriticLSTM(nn.Module):
-    def __init__(self, input_dim, lstm_hidden_dim, n_assets = num_assets):
-        super(ActorCriticLSTM, self).__init__()
+
+class Model(nn.Module):
+    def __init__(self,
+                 input_dim,
+                 n_assets=num_assets):
+        super(Model, self).__init__()
 
         self.input_dim = input_dim
-        self.lstm_hidden_dim = lstm_hidden_dim
+        self.lstm_hidden_dim = config['hyperparameters']['num_lstm_layers']
         self.n_assets = n_assets
         self.n_lstm_layers = config['hyperparameters']['num_lstm_layers']
 
         self.lstm = nn.LSTM(
-            input_size = input_dim,
-            hidden_size=lstm_hidden_dim,
+            input_size=input_dim,
+            hidden_size=self.lstm_hidden_dim,
             num_layers=self.n_lstm_layers,
-            batch_first=True
-        )
+            batch_first=True)
 
-        self.actor_head = nn.Linear(lstm_hidden_dim, n_assets * 2)
-        self.critic_head = nn.Linear(lstm_hidden_dim, 1)
+        self.actor_head = nn.Linear(self.lstm_hidden_dim, n_assets * 2)
+        self.critic_head = nn.Linear(self.lstm_hidden_dim, 1)
 
     def init_hidden_state(self, batch_size=1, device='cpu'):
         h_0 = torch.zeros(self.n_lstm_layers, batch_size, self.lstm_hidden_dim).to(device)
         c_0 = torch.zeros(self.n_lstm_layers, batch_size, self.lstm_hidden_dim).to(device)
         return (h_0, c_0)
 
-    def forward(self, x, hidden_state=None):
-
+    def forward(self,
+                x,
+                hidden_state=None):
         lstm_out, hidden_state_out = self.lstm(x, hidden_state)
         last_out = lstm_out[:, -1, :]
 
@@ -43,12 +46,14 @@ class ActorCriticLSTM(nn.Module):
 
         log_std = torch.clamp(log_std, -20, 4)
         stds = torch.exp(log_std)
-
         dist = Normal(means, stds)
 
         return dist, value, hidden_state_out
-    
-    def get_action_and_value(self, x, hidden_state=None, action=None):
+
+    def get_action_and_value(self,
+                             x,
+                             hidden_state=None,
+                             action=None):
         dist, value, hidden_state_out = self.forward(x, hidden_state)
 
         if action is None: